4c460590bcd5acf91947ece9001000d021812531
Pablo Romero <romeropablojr03@gmail.com>
2025-12-09 15:45:12 -0600

    readme

--- scripts/build.tcl
+++ scripts/build.tcl
@@ -1,16 +1,11 @@
-# read HDL
 read_verilog ./src/top.v
 
-# read constraints
-read_xdc ./constraints/basys3.xdc
+read_xdc ./constr/basys3.xdc
 
-# synthesize
 synth_design -top top -part xc7a35tcpg236-1
 
-# optimize, place, route
 opt_design
 place_design
 route_design
 
-# write bitstream
 write_bitstream -force top.bit
--- scripts/sim.tcl
+++ scripts/sim.tcl
@@ -1,9 +1,6 @@
-# compile HDL
 xvlog ./src/top.v
-xvlog ./src/top_tb.v
+xvlog ./sim/top_tb.v
 
-# elaborate (build simulation model)
 xelab top_tb -s sim_top
 
-# run simulation
 xsim sim_top -runall
